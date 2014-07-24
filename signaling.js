a4313a799832e06ffac1574ff50239898fbb9d11
root <root@server01.localdomain>
2014-07-25 02:04:09 +0900

    modify signaling.js

--- signaling.js
+++ signaling.js
@@ -56,6 +56,7 @@ io.sockets.on('connection', function(socket) {
 		 
 	socket.on('disconnect', function(message) {
 		message.from = socket.id;
+		console.log("message from: " + message.from);
 		emitMessage('disconnected', message);
 	});
 
