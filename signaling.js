60272816ab6394bcb38325e591f4580a0114d229
root <root@server01.localdomain>
2014-07-24 21:03:22 +0900

    modify signaling.js

--- signaling.js
+++ signaling.js
@@ -55,8 +55,8 @@ io.sockets.on('connection', function(socket) {
 	});
 		 
 	socket.on('disconnect', function() {
-		// socket.broadcast.emit('user disconnected');
-		emitMessage('user disconnected', message);
+		socket.broadcast.to(socket.roomName).emit('user disconnected');
+		//emitMessage('user disconnected');
 		console.log('disconnect');
 	});
 
