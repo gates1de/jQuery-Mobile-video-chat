3321aaccd5dcbd3dbd3602de6923bc0416eed5e7
root <root@server01.localdomain>
2014-07-24 23:54:42 +0900

    modify signaling.js

--- signaling.js
+++ signaling.js
@@ -56,7 +56,7 @@ io.sockets.on('connection', function(socket) {
 		 
 	socket.on('disconnect', function(message) {
 		//io.sockets.emit('disconnect');
-		//socket.broadcast.to(socket.roomName).emit('disconnect');
+		socket.broadcast.to(socket.roomName).emit('disconnect');
 		//emitMessage('user disconnected', message);
 		console.log('disconnect room name = ' + socket.roomName);
 	});
